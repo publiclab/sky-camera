f1e783e8a95fe7faa254adff91a11a3d12ae90c7
mohit216 <mohitiniitd@gmail.com>
2013-09-12 03:19:02 +0530

    App name has been changed to Sky Cam

--- src/com/org/tlapscam/SavePic.java
+++ src/com/org/tlapscam/SavePic.java
@@ -88,20 +88,15 @@ public class SavePic extends AsyncTask<PicData, Void, String> {
 	
 	public static int calculateInSampleSize(
 	        BitmapFactory.Options options, int reqWidth, int reqHeight) {
-	    // Raw height and width of image
 	    final int height = options.outHeight;
 	    final int width = options.outWidth;
-	    int inSampleSize = 1;
+	    int inSampleSize = 8;
 
 	    if (height > reqHeight || width > reqWidth) {
 
-	        // Calculate ratios of height and width to requested height and width
 	        final int heightRatio = Math.round((float) height / (float) reqHeight);
 	        final int widthRatio = Math.round((float) width / (float) reqWidth);
 
-	        // Choose the smallest ratio as inSampleSize value, this will guarantee
-	        // a final image with both dimensions larger than or equal to the
-	        // requested height and width.
 	        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
 	    }
 	    
